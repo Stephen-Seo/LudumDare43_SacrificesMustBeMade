89955a4eef668901ae9b5c32b1f48e8945e0ed27
Stephen Seo <seo.disparate@gmail.com>
2018-12-01 11:21:16 +0900

    More init stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,35 @@
 #include <SFML/Graphics.hpp>
+#include <GDT/GameLoop.hpp>
 
 int main(int argc, char** argv)
 {
+    const char* title = "LudumDare43 - Sacrifices must be made - "
+        "Entry by Stephen \"BurnedKirby\" Seo";
     sf::RenderWindow window(
         sf::VideoMode(480, 270),
-        "LudumDare43 - Sacrifices must be made",
+        title,
         sf::Style::Close | sf::Style::Titlebar);
 
+    bool isRunning = true;
+    GDT::IntervalBasedGameLoop(
+        &isRunning,
+        [&isRunning, &window] (float dt) { // update fn
+            sf::Event event;
+            while(window.pollEvent(event))
+            {
+                if(event.type == sf::Event::Closed)
+                {
+                    isRunning = false;
+                }
+            }
+        },
+        [&window] () { // draw fn
+            window.clear();
 
+            window.display();
+        },
+        60,
+        1.0f / 120.0f);
 
     window.close();
     return 0;
